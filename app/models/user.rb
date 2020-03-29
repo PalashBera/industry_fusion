7490d33df4accde139e85c9ba60cdb045f66eb0b
Palash Bera <palashbera1234@gmail.com>
2020-03-29 07:56:26 +0530

    Fixing before validation callback for user (#10)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
-  before_validation { self.first_name = first_name.squish.titleize }
-  before_validation { self.last_name = last_name.squish.titleize }
-  before_validation { self.email = email.squish.downcase }
-  before_validation { self.mobile_number = mobile_number.squish }
+  before_validation { self.first_name = first_name.to_s.squish.titleize }
+  before_validation { self.last_name = last_name.to_s.squish.titleize }
+  before_validation { self.email = email.to_s.squish.downcase }
+  before_validation { self.mobile_number = mobile_number.to_s.squish }
 
   validates :first_name, :last_name, presence: true, length: { maximum: 255 }
   validates :mobile_number, presence: true, length: { is: 10 }
