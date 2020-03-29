81b4a4693d814f6d9b03f1194ec142a3b9e6af33
Palash Bera <palashbera1234@gmail.com>
2020-03-30 00:10:11 +0530

    Adding spec for archivable model concern

--- spec/models/brand_spec.rb
+++ spec/models/brand_spec.rb
@@ -3,6 +3,8 @@ require "rails_helper"
 RSpec.describe Brand, type: :model do
   let!(:brand) { create(:brand, name: "Industry Fusion") }
 
+  it_behaves_like "archivable"
+
   describe "active record columns" do
     it { should have_db_column(:name) }
     it { should have_db_column(:archive) }
@@ -33,7 +35,6 @@ RSpec.describe Brand, type: :model do
     it { should validate_presence_of(:name) }
     it { should validate_length_of(:name).is_at_most(255) }
     it { should validate_uniqueness_of(:name).case_insensitive }
-    it { should validate_inclusion_of(:archive).in_array([true, false]) }
   end
 
   describe "scopes" do
