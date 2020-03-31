225a9aee1e427ba5f6f5b26a9accdc0f7378bc70
Palash Bera <palashbera1234@gmail.com>
2020-03-31 21:00:58 +0530

    Tracking change log for brands

--- config/initializers/paper_trail.rb
+++ config/initializers/paper_trail.rb
@@ -1,7 +1,1 @@
-PaperTrail.serializer = PaperTrail::Serializers::JSON
-
-PaperTrail::Version.class_eval do
-  def changed_object
-    @changed_object ||= JSON.parse(object, object_class: OpenStruct)
-  end
-end
+PaperTrail.config.has_paper_trail_defaults = { on: [:update] }
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 
   devise_for :users
-  resources :brands, except: [:show]
+
+  resources :brands, except: [:show] do
+    get :change_logs, on: :member
+  end
+
   root "home#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
