547af30fb617d800a1720c31a512ab3dbcf6e6c8
Palash Bera <palashbera1234@gmail.com>
2020-04-01 20:50:01 +0530

    Adding validation for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,16 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :async, :confirmable
+  devise :database_authenticatable, :registerable, :recoverable,
+         :rememberable, :validatable, :async, :confirmable, :trackable
 
   before_validation { self.first_name = first_name.to_s.squish.titleize }
   before_validation { self.last_name = last_name.to_s.squish.titleize }
   before_validation { self.email = email.to_s.squish.downcase }
   before_validation { self.mobile_number = mobile_number.to_s.squish }
 
-  validates :first_name, :last_name, presence: true, length: { maximum: 255 }
+  validates :first_name, :last_name, :email, presence: true, length: { maximum: 255 }
+  validates :password, presence: true, length: { minimum: 6, maximum: 128 }, on: :create
   validates :mobile_number, presence: true, length: { is: 10 }
 
   def full_name
