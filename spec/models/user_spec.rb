e082b4548c027a56870b703d5de23124c84134f5
Palash Bera <palashbera1234@gmail.com>
2020-04-03 23:23:20 +0530

    Adding specs for User model (#23)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -68,10 +68,14 @@ RSpec.describe User, type: :model do
   describe "validations" do
     it { should validate_presence_of(:first_name) }
     it { should validate_presence_of(:last_name) }
+    it { should validate_presence_of(:email) }
     it { should validate_presence_of(:mobile_number) }
+    it { should validate_presence_of(:password).on(:create) }
     it { should validate_length_of(:first_name).is_at_most(255) }
     it { should validate_length_of(:last_name).is_at_most(255) }
+    it { should validate_length_of(:email).is_at_most(255) }
     it { should validate_length_of(:mobile_number).is_equal_to(10) }
+    it { should validate_length_of(:password).is_at_least(6).is_at_most(128) }
   end
 
   describe "#full_name" do
