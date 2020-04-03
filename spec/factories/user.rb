d3d9c8d09bb4d26d61c8cb5e2c6600ffd91678ae
Palash Bera <palashbera1234@gmail.com>
2020-04-03 20:04:52 +0530

    Adding specs for User model

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -4,5 +4,6 @@ FactoryBot.define do
     last_name     { Faker::Name.last_name }
     mobile_number { Faker::PhoneNumber.subscriber_number(length: 10) }
     email         { Faker::Internet.email }
+    password      { Faker::Internet.password(min_length: 6, max_length: 128) }
   end
 end
