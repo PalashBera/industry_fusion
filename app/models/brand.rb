21a52e46a8b62a106ccd6744e4c426e36e9d1bd3
Arnab Roy <rarnab021@gmail.com>
2020-04-04 17:06:57 +0530

    Adding sort feature for brands

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -8,6 +8,4 @@ class Brand < ApplicationRecord
   before_validation { self.name = name.to_s.squish }
 
   validates :name, presence: true, length: { maximum: 255 }, uniqueness: { case_sensitive: false }
-
-  scope :order_by_name, -> { order(:name) }
 end
