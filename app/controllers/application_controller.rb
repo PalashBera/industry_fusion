f9d7c050e11be3894d822555facc009d01917daf
Palash Bera <palashbera1234@gmail.com>
2020-04-05 01:55:24 +0530

    Adding after login redirection for new organization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,4 +17,12 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name mobile_number])
     devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name mobile_number])
   end
+
+  def after_sign_in_path_for(resource)
+    if current_organization
+      stored_location_for(resource) || root_path
+    else
+      new_organization_path
+    end
+  end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,4 +6,8 @@ module SessionsHelper
   def non_admin_user?
     !admin_user?
   end
+
+  def current_organization
+    @current_organization ||= current_user&.organization
+  end
 end
