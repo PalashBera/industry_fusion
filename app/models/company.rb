217036e132f3a66d2cee2c64b40204817fbb9dc8
Palash Bera <palashbera1234@gmail.com>
2020-04-08 23:47:15 +0530

    Adding act_as_tenant to company model

--- app/models/company.rb
+++ app/models/company.rb
@@ -3,6 +3,7 @@ class Company < ApplicationRecord
   include Archivable
   include UserTrackable
 
+  acts_as_tenant(:organization)
   has_paper_trail ignore: %i[created_at updated_at]
 
   before_validation do
