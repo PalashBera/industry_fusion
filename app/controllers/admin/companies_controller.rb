7ca496f7fa6744d70e9868bc51f73e3210433237
Palash Bera <palashbera1234@gmail.com>
2020-04-08 23:25:09 +0530

    Adding controller for admin companies

---
+++ app/controllers/admin/companies_controller.rb
@@ -0,0 +1,49 @@
+class Admin::CompaniesController < Admin::AdminController
+  include ChangeLogable
+
+  def index
+    @search = Company.kept.ransack(params[:q])
+    @pagy, @companies = pagy(@search.result, items: 20)
+  end
+
+  def new
+    @company = Company.new
+  end
+
+  def create
+    @company = Company.new(company_params)
+
+    if @company.save
+      redirect_to admin_companies_path, flash: { success: "Company has been successfully created." }
+    else
+      render "new"
+    end
+  end
+
+  def edit
+    company
+  end
+
+  def update
+    if company.update(company_params)
+      redirect_to admin_companies_path, flash: { success: "Company has been successfully updated." }
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    company.discard
+    redirect_to admin_companies_path, flash: { danger: "Company has been successfully deleted." }
+  end
+
+  private
+
+  def company_params
+    params.require(:company).permit(:name, :address1, :address2, :city, :state, :country, :pin_code, :phone_number, :archive)
+  end
+
+  def company
+    @company ||= Company.find(params[:id])
+  end
+end
