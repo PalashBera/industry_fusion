4b94dab2cd972e1c8dba8f51ab8b8eb6fe50e3cd
Arnab Roy <rarnab021@gmail.com>
2020-04-10 11:41:04 +0530

    Adding export option in brands

--- config/routes.rb
+++ config/routes.rb
@@ -31,5 +31,5 @@ Rails.application.routes.draw do
   end
 
   root "home#index"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.htmel
 end
