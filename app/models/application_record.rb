a0a70f33b8ef9588fae10375307c8ff96019b554
Palash Bera <palashbera1234@gmail.com>
2020-04-11 14:32:49 +0530

    Adding CSV import option to brands

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,11 @@
+require "csv"
+
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def self.import(file)
+    CSV.foreach(file.path, headers: true) do |row|
+      create(row.to_h)
+    end
+  end
 end
