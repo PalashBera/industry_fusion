d0a8ab3b439bd67f3fb3ca6053ce34eb9a7ddd8a
Palash Bera <palashbera1234@gmail.com>
2020-04-11 14:32:49 +0530

    Adding CSV import option to brands

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -13,23 +13,4 @@ class Brand < ApplicationRecord
   validates :name, presence: true, length: { maximum: 255 }, uniqueness: { case_sensitive: false, scope: :organization_id }
 
   scope :order_by_name, -> { order(:name) }
-
-  def self.import(file)
-    spreadsheet = open_spreadsheet(file)
-
-    (2..spreadsheet.last_row).each do |i|
-      brand = new
-      brand.name = spreadsheet.row(i)[0]
-      brand.save
-    end
-  end
-
-  def self.open_spreadsheet(file)
-    case File.extname(file.original_filename)
-    when ".csv" then Csv.new(file.path, nil, :ignore)
-    when ".xls" then Roo::Excel.new(file.path, nil, :ignore)
-    when ".xlsx" then Roo::Excelx.new(file.path)
-    else raise StandardError, "Unknown file type: #{file.original_filename}"
-    end
-  end
 end
