9c277b44477a0e40207a37be454eaa7e1c685d4b
Palash Bera <palashbera1234@gmail.com>
2020-04-11 22:59:04 +0530

    Adding warehouses controller and routes

---
+++ app/controllers/admin/warehouses_controller.rb
@@ -0,0 +1,43 @@
+class Admin::WarehousesController < Admin::AdminController
+  def index
+    @search = Warehouse.ransack(params[:q])
+    @search.sorts = "name asc" if @search.sorts.empty?
+    @pagy, @warehouses = pagy(@search.result, items: 20)
+  end
+
+  def new
+    @warehouse = Warehouse.new
+  end
+
+  def create
+    @warehouse = Warehouse.new(warehouse_params)
+
+    if @warehouse.save
+      redirect_to admin_warehouses_path, flash: { success: "Warehouse has been successfully created." }
+    else
+      render "new"
+    end
+  end
+
+  def edit
+    warehouse
+  end
+
+  def update
+    if warehouse.update(warehouse_params)
+      redirect_to admin_warehouses_path, flash: { success: "Warehouse has been successfully updated." }
+    else
+      render "edit"
+    end
+  end
+
+  private
+
+  def warehouse_params
+    params.require(:warehouse).permit(:company_id, :name, :address1, :address2, :city, :state, :country, :pin_code, :phone_number, :archive)
+  end
+
+  def warehouse
+    @warehouse ||= Warehouse.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,8 @@ Rails.application.routes.draw do
       get :export, on: :collection
     end
 
+    resources :warehouses, except: [:show, :destroy]
+
     root "admin#index"
   end
 
