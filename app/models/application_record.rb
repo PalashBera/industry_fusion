6d5be39af56087e5fc7445c4b4818a84bcbc123e
Arnab Roy <rarnab021@gmail.com>
2020-04-12 14:08:16 +0530

    Adding export and import feature for uom

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -4,7 +4,7 @@ class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
   def self.import(file)
-    CSV.foreach(file.path, headers: true) do |row|
+    CSV.foreach(file.path, headers: true, header_converters: :symbol) do |row|
       create(row.to_h)
     end
   end
