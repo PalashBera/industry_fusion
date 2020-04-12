deca4f15670234f84ead7d3e308c41d4ba2573f0
Palash Bera <palashbera1234@gmail.com>
2020-04-12 19:07:31 +0530

    Adding nameable concern to model

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -2,15 +2,10 @@ class Brand < ApplicationRecord
   include Archivable
   include ModalFormable
   include UserTrackable
+  include Nameable
 
   acts_as_tenant(:organization)
   has_paper_trail ignore: %i[created_at updated_at]
 
-  before_validation { self.name = name.to_s.squish }
-
   belongs_to :organization
-
-  validates :name, presence: true, length: { maximum: 255 }, uniqueness: { case_sensitive: false, scope: :organization_id }
-
-  scope :order_by_name, -> { order(:name) }
 end
--- app/models/uom.rb
+++ app/models/uom.rb
@@ -2,20 +2,16 @@ class Uom < ApplicationRecord
   include Archivable
   include ModalFormable
   include UserTrackable
+  include Nameable
 
   acts_as_tenant(:organization)
   has_paper_trail ignore: %i[created_at updated_at]
 
-  before_validation do
-    self.name = name.to_s.squish
-    self.short_name = short_name.to_s.squish
-  end
+  before_validation { self.short_name = short_name.to_s.squish }
 
   belongs_to :organization
 
-  validates :name, presence: true, length: { maximum: 255 }, uniqueness: { case_sensitive: false, scope: :organization_id }
   validates :short_name, presence: true, length: { maximum: 4 }, uniqueness: { case_sensitive: false, scope: :organization_id }
 
-  scope :order_by_name, -> { order(:name) }
   scope :order_by_short_name, -> { order(:short_name) }
 end
