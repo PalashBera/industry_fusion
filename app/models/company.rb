ba6f271d057edd5540748e08b7f1d736f5f36c4f
Palash Bera <palashbera1234@gmail.com>
2020-04-14 17:35:32 +0530

    Adding addressable concern to model

--- app/models/company.rb
+++ app/models/company.rb
@@ -3,25 +3,12 @@ class Company < ApplicationRecord
   include Archivable
   include UserTrackable
   include Nameable
+  include Addressable
 
   acts_as_tenant(:organization)
   has_paper_trail ignore: %i[created_at updated_at]
 
-  before_validation do
-    self.address1 = address1.to_s.squish
-    self.address2 = address2.to_s.squish
-    self.city = city.to_s.squish.titleize
-    self.state = state.to_s.squish.titleize
-    self.country = country.to_s.squish.titleize
-    self.pin_code = pin_code.to_s.squish
-    self.phone_number = phone_number.to_s.squish
-  end
-
   belongs_to :organization
 
   has_many :warehouses
-
-  validates :address1, :city, :state, :country, presence: true, length: { maximum: 255 }
-  validates :address2, :phone_number, length: { maximum: 255 }
-  validates :pin_code, presence: true, length: { is: 6 }
 end
--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,19 +1,11 @@
 class Organization < ApplicationRecord
   include Archivable
   include UserTrackable
+  include Addressable
 
   has_paper_trail ignore: %i[created_at updated_at]
 
-  before_validation do
-    self.name = name.to_s.squish
-    self.address1 = address1.to_s.squish
-    self.address2 = address2.to_s.squish
-    self.city = city.to_s.squish.titleize
-    self.state = state.to_s.squish.titleize
-    self.country = country.to_s.squish.titleize
-    self.pin_code = pin_code.to_s.squish
-    self.phone_number = phone_number.to_s.squish
-  end
+  before_validation { self.name = name.to_s.squish }
 
   has_many :users
   has_many :companies
@@ -24,9 +16,6 @@ class Organization < ApplicationRecord
   has_many :cost_centers
 
   validates :name, presence: true, length: { maximum: 255 }, uniqueness: { case_sensitive: false }
-  validates :address1, :city, :state, :country, presence: true, length: { maximum: 255 }
-  validates :address2, :phone_number, length: { maximum: 255 }
-  validates :pin_code, presence: true, length: { is: 6 }
 
   scope :order_by_name, -> { order(:name) }
 end
