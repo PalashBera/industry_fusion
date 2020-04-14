eb4c4922513e4a687cc08ce66bbac2ad6af6b54f
Palash Bera <palashbera1234@gmail.com>
2020-04-14 18:49:59 +0530

    Adding addressable model concern specs

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -3,15 +3,10 @@ require "rails_helper"
 RSpec.describe Organization, type: :model do
   let!(:organization) { create(:organization, name: "Industry Fusion") }
 
+  it_behaves_like "addressable"
+
   describe "active record columns" do
     it { should have_db_column(:name) }
-    it { should have_db_column(:address1) }
-    it { should have_db_column(:address2) }
-    it { should have_db_column(:city) }
-    it { should have_db_column(:state) }
-    it { should have_db_column(:country) }
-    it { should have_db_column(:pin_code) }
-    it { should have_db_column(:description) }
     it { should have_db_column(:archive) }
     it { should have_db_column(:created_by_id) }
     it { should have_db_column(:updated_by_id) }
@@ -32,62 +27,6 @@ RSpec.describe Organization, type: :model do
         expect(organization.name).to eq ("Industry Fusion")
       end
     end
-
-    context "when address1 contains extra space" do
-      it "should remove extra space" do
-        organization = build(:organization, address1: " Salt Lake City ")
-        organization.valid?
-        expect(organization.address1).to eq ("Salt Lake City")
-      end
-    end
-
-    context "when address2 contains extra space" do
-      it "should remove extra space" do
-        organization = build(:organization, address2: " Salt Lake City ")
-        organization.valid?
-        expect(organization.address2).to eq ("Salt Lake City")
-      end
-    end
-
-    context "when city contains extra space and is not titleize" do
-      it "should remove extra space and make titleize" do
-        organization = build(:organization, city: " kolkata  ")
-        organization.valid?
-        expect(organization.city).to eq ("Kolkata")
-      end
-    end
-
-    context "when state contains extra space and is not titleize" do
-      it "should remove extra space and make titleize" do
-        organization = build(:organization, state: " west  Bengal  ")
-        organization.valid?
-        expect(organization.state).to eq ("West Bengal")
-      end
-    end
-
-    context "when country contains extra space and is not titleize" do
-      it "should remove extra space and make titleize" do
-        organization = build(:organization, country: " india  ")
-        organization.valid?
-        expect(organization.country).to eq ("India")
-      end
-    end
-
-    context "when pin_code contains extra space" do
-      it "should remove extra space" do
-        organization = build(:organization, pin_code: " 700091 ")
-        organization.valid?
-        expect(organization.pin_code).to eq ("700091")
-      end
-    end
-
-    context "when description contains extra space" do
-      it "should remove extra space" do
-        organization = build(:organization, description: " Produces a number with 2 digits leading    and   trailing the decimal ")
-        organization.valid?
-        expect(organization.description).to eq ("Produces a number with 2 digits leading and trailing the decimal")
-      end
-    end
   end
 
   describe "associations" do
@@ -98,18 +37,7 @@ RSpec.describe Organization, type: :model do
 
   describe "validations" do
     it { should validate_presence_of(:name) }
-    it { should validate_presence_of(:address1) }
-    it { should validate_presence_of(:city) }
-    it { should validate_presence_of(:state) }
-    it { should validate_presence_of(:country) }
-    it { should validate_presence_of(:pin_code) }
     it { should validate_length_of(:name).is_at_most(255) }
-    it { should validate_length_of(:address1).is_at_most(255) }
-    it { should validate_length_of(:address2).is_at_most(255) }
-    it { should validate_length_of(:city).is_at_most(255) }
-    it { should validate_length_of(:state).is_at_most(255) }
-    it { should validate_length_of(:country).is_at_most(255) }
-    it { should validate_length_of(:pin_code).is_equal_to(6) }
     it { should validate_uniqueness_of(:name).case_insensitive }
   end
 
---
+++ spec/support/concerns/addressable_spec.rb
@@ -0,0 +1,86 @@
+require "rails_helper"
+
+shared_examples_for "addressable" do
+  describe "active record columns" do
+    it { should have_db_column(:address1) }
+    it { should have_db_column(:address2) }
+    it { should have_db_column(:city) }
+    it { should have_db_column(:state) }
+    it { should have_db_column(:country) }
+    it { should have_db_column(:pin_code) }
+    it { should have_db_column(:phone_number) }
+  end
+
+  describe "callbacks" do
+    context "when address1 contains extra space" do
+      it "should remove extra space" do
+        resource = build(described_class.to_s.underscore.to_sym, address1: " Salt Lake City ")
+        resource.valid?
+        expect(resource.address1).to eq ("Salt Lake City")
+      end
+    end
+
+    context "when address2 contains extra space" do
+      it "should remove extra space" do
+        resource = build(described_class.to_s.underscore.to_sym, address2: " Salt Lake City ")
+        resource.valid?
+        expect(resource.address2).to eq ("Salt Lake City")
+      end
+    end
+
+    context "when city contains extra space and is not titleize" do
+      it "should remove extra space and make titleize" do
+        resource = build(described_class.to_s.underscore.to_sym, city: " kolkata  ")
+        resource.valid?
+        expect(resource.city).to eq ("Kolkata")
+      end
+    end
+
+    context "when state contains extra space and is not titleize" do
+      it "should remove extra space and make titleize" do
+        resource = build(described_class.to_s.underscore.to_sym, state: " west  Bengal  ")
+        resource.valid?
+        expect(resource.state).to eq ("West Bengal")
+      end
+    end
+
+    context "when country contains extra space and is not titleize" do
+      it "should remove extra space and make titleize" do
+        resource = build(described_class.to_s.underscore.to_sym, country: " india  ")
+        resource.valid?
+        expect(resource.country).to eq ("India")
+      end
+    end
+
+    context "when pin_code contains extra space" do
+      it "should remove extra space" do
+        resource = build(described_class.to_s.underscore.to_sym, pin_code: " 700091 ")
+        resource.valid?
+        expect(resource.pin_code).to eq ("700091")
+      end
+    end
+
+    context "when phone_number contains extra space" do
+      it "should remove extra space" do
+        resource = build(described_class.to_s.underscore.to_sym, phone_number: " +033  2585  2565 ")
+        resource.valid?
+        expect(resource.phone_number).to eq ("+033 2585 2565")
+      end
+    end
+  end
+
+  describe "validations" do
+    it { should validate_presence_of(:address1) }
+    it { should validate_presence_of(:city) }
+    it { should validate_presence_of(:state) }
+    it { should validate_presence_of(:country) }
+    it { should validate_presence_of(:pin_code) }
+    it { should validate_length_of(:address1).is_at_most(255) }
+    it { should validate_length_of(:address2).is_at_most(255) }
+    it { should validate_length_of(:city).is_at_most(255) }
+    it { should validate_length_of(:state).is_at_most(255) }
+    it { should validate_length_of(:country).is_at_most(255) }
+    it { should validate_length_of(:phone_number).is_at_most(255) }
+    it { should validate_length_of(:pin_code).is_equal_to(6) }
+  end
+end
