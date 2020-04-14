fa592c92e6f802dc72020a5d1259db1c72ea30fe
Palash Bera <palashbera1234@gmail.com>
2020-04-14 19:28:01 +0530

    Adding specs for warehouse model

---
+++ spec/factories/warehouse.rb
@@ -0,0 +1,15 @@
+FactoryBot.define do
+  factory :warehouse do
+    name         { Faker::Company.industry }
+    address1     { Faker::Address.street_address }
+    address2     { Faker::Address.secondary_address }
+    city         { Faker::Address.city }
+    state        { Faker::Address.state }
+    country      { Faker::Address.country }
+    pin_code     { Faker::Number.number(digits: 6) }
+    phone_number { Faker::PhoneNumber.phone_number }
+    archive      { false }
+    company
+    organization
+  end
+end
---
+++ spec/models/warehouse_spec.rb
@@ -0,0 +1,33 @@
+require "rails_helper"
+
+RSpec.describe Warehouse, type: :model do
+  let(:organization) { create(:organization) }
+  let(:company)      { create(:company, organization: organization) }
+  let!(:warehouse)   { create(:warehouse, name: "Industry Fusion Warehouse", organization: organization, company: company) }
+
+  it_behaves_like "modal_formable"
+  it_behaves_like "archivable"
+  it_behaves_like "nameable"
+  it_behaves_like "addressable"
+
+  describe "active record columns" do
+    it { should have_db_column(:company_id) }
+    it { should have_db_column(:organization_id) }
+    it { should have_db_column(:created_by_id) }
+    it { should have_db_column(:updated_by_id) }
+    it { should have_db_column(:created_at) }
+    it { should have_db_column(:updated_at) }
+  end
+
+  describe "active record index" do
+    it { should have_db_index(:company_id) }
+    it { should have_db_index(:organization_id) }
+    it { should have_db_index(:created_by_id) }
+    it { should have_db_index(:updated_by_id) }
+  end
+
+  describe "associations" do
+    it { should belong_to(:organization) }
+    it { should belong_to(:company) }
+  end
+end
