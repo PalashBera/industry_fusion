7a63a8f33060cb8d1b5b11df1ffa13a7e75a6b20
Palash Bera <palashbera1234@gmail.com>
2020-04-14 19:23:51 +0530

    Adding specs for company model

--- spec/factories/cost_center.rb
+++ spec/factories/cost_center.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :cost_center do
-    name        { Faker::Lorem.word + rand(9999).to_s }
-    description { Faker::Marketing.buzzwords }
-    archive     { false }
+    name         { Faker::Lorem.word + rand(9999).to_s }
+    description  { Faker::Marketing.buzzwords }
+    archive      { false }
     organization
   end
 end
