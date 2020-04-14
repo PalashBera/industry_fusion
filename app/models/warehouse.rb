d78cab29ea88e29975cbe0f8f7e0aa53f050259b
Palash Bera <palashbera1234@gmail.com>
2020-04-14 17:35:32 +0530

    Adding addressable concern to model

--- app/models/warehouse.rb
+++ app/models/warehouse.rb
@@ -3,26 +3,13 @@ class Warehouse < ApplicationRecord
   include Archivable
   include UserTrackable
   include Nameable
+  include Addressable
 
   acts_as_tenant(:organization)
   has_paper_trail ignore: %i[created_at updated_at]
 
-  before_validation do
-    self.address1 = address1.to_s.squish
-    self.address2 = address2.to_s.squish
-    self.city = city.to_s.squish.titleize
-    self.state = state.to_s.squish.titleize
-    self.country = country.to_s.squish.titleize
-    self.pin_code = pin_code.to_s.squish
-    self.phone_number = phone_number.to_s.squish
-  end
-
   belongs_to :organization
   belongs_to :company
-
-  validates :address1, :city, :state, :country, presence: true, length: { maximum: 255 }
-  validates :address2, :phone_number, length: { maximum: 255 }
-  validates :pin_code, presence: true, length: { is: 6 }
 
   def self.included_resources
     includes(:company)
