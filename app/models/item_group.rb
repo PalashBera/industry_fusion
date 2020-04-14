5dbdb3470dd6427ceec7e403aeef205aaf72f0b1
Palash Bera <palashbera1234@gmail.com>
2020-04-14 17:32:16 +0530

    Adding nameable concern to cost center and item group

--- app/models/item_group.rb
+++ app/models/item_group.rb
@@ -2,17 +2,10 @@ class ItemGroup < ApplicationRecord
   include Archivable
   include ModalFormable
   include UserTrackable
+  include Nameable
 
   acts_as_tenant(:organization)
   has_paper_trail ignore: %i[created_at updated_at]
 
-  before_validation do
-    self.name = name.to_s.squish
-  end
-
   belongs_to :organization
-
-  validates :name, presence: true, length: { maximum: 255 }, uniqueness: { case_sensitive: false, scope: :organization_id }
-
-  scope :order_by_name, -> { order(:name) }
 end
