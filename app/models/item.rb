03d5bdc3712e77f17376bc2552b7300003e02589
Palash Bera <palashbera1234@gmail.com>
2020-04-18 12:36:54 +0530

    Redesining item modal form

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,15 +7,13 @@ class Item < ApplicationRecord
   acts_as_tenant(:organization)
   has_paper_trail ignore: %i[created_at updated_at]
 
-  before_save :remove_quantities_if_secondary_uom_absent
-
   belongs_to :organization
   belongs_to :item_group
   belongs_to :uom
   belongs_to :secondary_uom, class_name: "Uom", optional: true
 
-  validates :primary_quantity, :secondary_quantity, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 10_000_000, allow_nil: true }
-  validate :secondary_uom_value
+  validates :primary_quantity, :secondary_quantity, presence: true, numericality: { greater_than: 0 }, if: :secondary_uom
+  validate :check_secondary_uom_equality
 
   def self.included_resources
     includes(:item_group, :uom, :secondary_uom)
@@ -23,14 +21,7 @@ class Item < ApplicationRecord
 
   private
 
-  def remove_quantities_if_secondary_uom_absent
-    return unless secondary_uom.blank?
-
-    self.primary_quantity = nil
-    self.secondary_quantity = nil
-  end
-
-  def secondary_uom_value
-    errors.add(:secondary_uom, "can't be same as UOM") if secondary_uom == uom
+  def check_secondary_uom_equality
+    errors.add(:secondary_uom, "can't be same as UOM") if uom == secondary_uom
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,14 @@
-2.times do
-  Organization.create!(
-    name: Faker::Company.industry,
-    address1: Faker::Address.building_number + " " + Faker::Address.street_name,
-    address2: Faker::Address.secondary_address + " " + Faker::Address.street_address,
-    city: Faker::Address.city,
-    state: Faker::Address.state,
-    country: Faker::Address.country,
-    pin_code: Faker::Number.number(digits: 6).to_s
-  )
-end
+organization = Organization.create!(
+  name: "Industry Fusion Pvt. Ltd.",
+  address1: Faker::Address.building_number + ", " + Faker::Address.street_name,
+  address2: Faker::Address.secondary_address + ", " + Faker::Address.street_address,
+  city: Faker::Address.city,
+  state: Faker::Address.state,
+  country: Faker::Address.country,
+  pin_code: Faker::Number.number(digits: 6).to_s
+)
 
-User.create!(
+user1 = User.create!(
   first_name: "Palash",
   last_name: "Bera",
   mobile_number: "1234567890",
@@ -20,10 +18,10 @@ User.create!(
   admin: true,
   confirmation_sent_at: Time.current,
   confirmed_at: Time.current + 5.minutes,
-  organization_id: 1
+  organization_id: organization.id
 )
 
-User.create!(
+user2 = User.create!(
   first_name: "Arnab",
   last_name: "Roy",
   mobile_number: "9931148514",
@@ -33,98 +31,86 @@ User.create!(
   admin: true,
   confirmation_sent_at: Time.current,
   confirmed_at: Time.current + 5.minutes,
-  organization_id: 2
+  organization_id: organization.id
 )
 
-[1, 2].each do |org_id|
-  organization = Organization.find(org_id)
+5.times do |t|
+  User.current_user = [user1, user2].sample
 
-  5.times do
-    Company.create!(
-      name: Faker::Company.industry + " #" + rand(99_999).to_s,
-      address1: Faker::Address.building_number + " " + Faker::Address.street_name,
-      address2: Faker::Address.secondary_address + " " + Faker::Address.street_address,
-      city: Faker::Address.city,
-      state: Faker::Address.state,
-      country: Faker::Address.country,
-      pin_code: Faker::Number.number(digits: 6).to_s,
-      phone_number: Faker::PhoneNumber.phone_number_with_country_code,
-      organization_id: org_id
-    )
-  end
+  company = Company.create!(
+    name: Faker::Company.name + "+" + t.to_s,
+    address1: Faker::Address.building_number + ", " + Faker::Address.street_name,
+    address2: Faker::Address.secondary_address + ", " + Faker::Address.street_address,
+    city: Faker::Address.city,
+    state: Faker::Address.state,
+    country: Faker::Address.country,
+    pin_code: Faker::Number.number(digits: 6).to_s,
+    phone_number: Faker::PhoneNumber.phone_number_with_country_code,
+    organization_id: organization.id
+  )
 
-  250.times do
-    Brand.create!(
-      name: Faker::Appliance.brand + " #" + rand(999).to_s + "-" + Faker::Esport.game + " ## " + rand(999).to_s,
-      archive: Faker::Boolean.boolean,
-      organization_id: org_id
-    )
-  end
-
-  Uom.create(name: "dozen",      short_name: "doz", archive: Faker::Boolean.boolean, organization_id: org_id)
-  Uom.create(name: "inch",       short_name: "in",  archive: Faker::Boolean.boolean, organization_id: org_id)
-  Uom.create(name: "gallon",     short_name: "gal", archive: Faker::Boolean.boolean, organization_id: org_id)
-  Uom.create(name: "yard",       short_name: "yd",  archive: Faker::Boolean.boolean, organization_id: org_id)
-  Uom.create(name: "milliliter", short_name: "ml",  archive: Faker::Boolean.boolean, organization_id: org_id)
-
-  5.times do
+  [2, 3, 4].sample.times do |tt|
     Warehouse.create!(
-      name: Faker::Commerce.department + "##" + rand(99).to_s,
-      address1: Faker::Address.building_number + " " + Faker::Address.street_name,
-      address2: Faker::Address.secondary_address + " " + Faker::Address.street_address,
+      name: Faker::Commerce.department + "+" + t.to_s + tt.to_s,
+      address1: Faker::Address.building_number + ", " + Faker::Address.street_name,
+      address2: Faker::Address.secondary_address + ", " + Faker::Address.street_address,
       city: Faker::Address.city,
       state: Faker::Address.state,
       country: Faker::Address.country,
       pin_code: Faker::Number.number(digits: 6).to_s,
       phone_number: Faker::PhoneNumber.phone_number_with_country_code,
-      company_id: Company.where(organization_id: org_id).pluck(:id).sample,
-      organization_id: org_id
+      company_id: company.id,
+      organization_id: organization.id
     )
-  end
-
-  250.times do
-    ItemGroup.create!(
-      name: Faker::Beer.name + " #" + rand(99).to_s + "-" + Faker::Lorem.word,
-      description: Faker::Lorem.sentence(word_count: 6, supplemental: true, random_words_to_add: 3),
-      archive: Faker::Boolean.boolean,
-      organization_id: org_id
-    )
-  end
-
-  150.times do
-    CostCenter.create!(
-      name: Faker::Lorem.word + " #" + rand(99).to_s + "-" + Faker::Lorem.word,
-      description: Faker::Lorem.sentence(word_count: 6, supplemental: true, random_words_to_add: 3),
-      archive: Faker::Boolean.boolean,
-      organization_id: org_id
-    )
-  end
-
-  200.times do
-    Item.create!(
-      name: Faker::Vehicle.make_and_model + " ## #{rand(999_999)} ",
-      archive: Faker::Boolean.boolean,
-      uom_id: organization.uoms.sample.id,
-      item_group_id: organization.item_groups.sample.id,
-      organization_id: org_id
-    )
-  end
-
-  if org_id == 1
-    Company.update_all(created_by_id: 1)
-    Brand.update_all(created_by_id: 1)
-    Uom.update_all(created_by_id: 1)
-    Warehouse.update_all(created_by_id: 1)
-    ItemGroup.update_all(created_by_id: 1)
-    CostCenter.update_all(created_by_id: 1)
-    Item.update_all(created_by_id: 1)
-  else
-    Company.where(created_by_id: nil).update_all(created_by_id: 2)
-    Brand.where(created_by_id: nil).update_all(created_by_id: 2)
-    Uom.where(created_by_id: nil).update_all(created_by_id: 2)
-    Warehouse.where(created_by_id: nil).update_all(created_by_id: 2)
-    ItemGroup.where(created_by_id: nil).update_all(created_by_id: 2)
-    CostCenter.where(created_by_id: nil).update_all(created_by_id: 2)
-    Item.where(created_by_id: nil).update_all(created_by_id: 2)
   end
 end
+
+50.times do |t|
+  User.current_user = [user1, user2].sample
+
+  Brand.create!(
+    name: Faker::Appliance.brand + " - " + Faker::Esport.game + "+" + t.to_s,
+    archive: Faker::Boolean.boolean,
+    organization_id: organization.id
+  )
+end
+
+User.current_user = [user1, user2].sample
+
+Uom.create(name: "dozen",      short_name: "doz", archive: Faker::Boolean.boolean, organization_id: organization.id)
+Uom.create(name: "inch",       short_name: "in",  archive: Faker::Boolean.boolean, organization_id: organization.id)
+Uom.create(name: "gallon",     short_name: "gal", archive: Faker::Boolean.boolean, organization_id: organization.id)
+Uom.create(name: "yard",       short_name: "yd",  archive: Faker::Boolean.boolean, organization_id: organization.id)
+Uom.create(name: "milliliter", short_name: "ml",  archive: Faker::Boolean.boolean, organization_id: organization.id)
+
+50.times do |t|
+  User.current_user = [user1, user2].sample
+
+  ItemGroup.create!(
+    name: Faker::Beer.name + "+" + t.to_s,
+    description: Faker::Lorem.sentence(word_count: 6, supplemental: true, random_words_to_add: 3),
+    archive: Faker::Boolean.boolean,
+    organization_id: organization.id
+  )
+
+  CostCenter.create!(
+    name: Faker::Team.name + "+" + t.to_s,
+    description: Faker::Lorem.sentence(word_count: 6, supplemental: true, random_words_to_add: 3),
+    archive: Faker::Boolean.boolean,
+    organization_id: organization.id
+  )
+end
+
+50.times do |t|
+  User.current_user = [user1, user2].sample
+
+  Item.create!(
+    name: Faker::Appliance.equipment + "+" + t.to_s,
+    archive: Faker::Boolean.boolean,
+    uom_id: organization.uoms.sample.id,
+    item_group_id: organization.item_groups.sample.id,
+    organization_id: organization.id
+  )
+end
+
+Organization.update_all(created_by_id: [user1, user2].sample.id)
