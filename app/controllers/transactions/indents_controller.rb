60de84345299543c2e8bbf4c5b8d67987454a656
ARNAB ROY <35267739+arnabry11@users.noreply.github.com>
2020-04-26 01:56:36 +0530

    Adding indent creation feature (#62)

--- app/controllers/transactions/indents_controller.rb
+++ app/controllers/transactions/indents_controller.rb
@@ -5,13 +5,35 @@ class Transactions::IndentsController < Transactions::TransactionsController
     @pagy, @indent_items = pagy_countless(@search.result.included_resources, link_extra: 'data-remote="true"')
   end
 
-  def new; end
+  def new
+    @indent = Indent.new
+    @indent_item = @indent.indent_items.build
+  end
 
-  def create; end
+  def create
+    @indent = Indent.new(indent_params)
+
+    if @indent.save
+      redirect_to transactions_indents_path, flash: { success: "Indent has been successfully created." }
+    else
+      render "new"
+    end
+  end
 
   def edit; end
 
   def update; end
+
+  def fetch_warehouses
+    @company = Company.find(params[:company_id])
+  end
+
+  def fetch_makes_and_uoms
+    item = Item.find(params[:item_id])
+    @unique_id = params[:unique_id]
+    @makes = item.makes.included_resources
+    @uoms = item.uoms_list
+  end
 
   private
 
@@ -20,6 +42,7 @@ class Transactions::IndentsController < Transactions::TransactionsController
   end
 
   def indent_params
-    params.require(:indent).permit(:date, :company_id, :warehouse_id, indent_item_attributes: %i[id item_id cost_center_id make_id uom_id quantity priority note])
+    params.require(:indent).permit(:requirement_date, :company_id, :warehouse_id,
+                                   indent_items_attributes: %i[id item_id cost_center_id make_id uom_id quantity priority note _destroy])
   end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("indents.js")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -22,9 +23,10 @@ document.addEventListener('turbolinks:load', function() {
 
   $('.se-pre-con').fadeOut('slow');
   SelectpickerHandler();
+  DatepickerHandler();
 
   var loadNextPage = function () {
-    if ($('#next_link').data("loading")) { return }  // prevent multiple loading
+    if ($('#records_table').height() === undefined || $('#next_link').data("loading")) { return }  // prevent multiple loading
     var wBottom = $(window).scrollTop() + $(window).height();
     var elBottom = $('#records_table').offset().top + $('#records_table').height();
     if (wBottom > elBottom) {
@@ -72,3 +74,12 @@ const SelectpickerHandler = function () {
 
   $('.selectpicker').selectpicker('refresh');
  }
+
+const DatepickerHandler = function () {
+  $('#datepicker').datepicker({
+    format: 'd-M-yyyy',
+    startDate: '-0d',
+    autoclose: true,
+    todayHighlight: true
+  });
+}
