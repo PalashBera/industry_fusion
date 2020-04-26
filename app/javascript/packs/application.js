c524fb96ac4e0e78250ecbf4a2bbbd287b8cba2f
ARNAB ROY <35267739+arnabry11@users.noreply.github.com>
2020-04-26 08:25:34 +0530

    Using preloader in index page and refactoring javascript code (#64)

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,22 +24,6 @@ document.addEventListener('turbolinks:load', function() {
   $('.se-pre-con').fadeOut('slow');
   SelectpickerHandler();
   DatepickerHandler();
-
-  var loadNextPage = function () {
-    if ($('#records_table').height() === undefined || $('#next_link').data("loading")) { return }  // prevent multiple loading
-    var wBottom = $(window).scrollTop() + $(window).height();
-    var elBottom = $('#records_table').offset().top + $('#records_table').height();
-    if (wBottom > elBottom) {
-      if ($('#next_link')[0] != undefined) {    // checking next page link is available or not
-        $('#next_link')[0].click();
-        $('#next_link').data("loading", true);
-      }
-    }
-  };
-
-  window.addEventListener('resize', loadNextPage);
-  window.addEventListener('scroll', loadNextPage);
-  window.addEventListener('load', loadNextPage);
 });
 
 $(document).on('click', '.remove-selectpicker-selections', function (e) {
