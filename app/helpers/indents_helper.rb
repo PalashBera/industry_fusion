9131599f011f27d2b7c6d566a53a421afb385000
ARNAB ROY <35267739+arnabry11@users.noreply.github.com>
2020-04-27 18:35:23 +0530

    Changing bootstrap datepicker (#67)

--- app/helpers/indents_helper.rb
+++ app/helpers/indents_helper.rb
@@ -10,4 +10,8 @@ module IndentsHelper
   def uom_options(object)
     Item.find_by(id: object.item_id)&.uoms.to_a.map { |uom| [uom.short_name, uom.id] }
   end
+
+  def format_requirement_date(object)
+    object.id.present? ? object.requirement_date.strftime("%d-%b-%Y") : ""
+  end
 end
