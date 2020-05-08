487ffac1f675207d59c247d58caac8b0412a9e1c
Palash Bera <palashbera1234@gmail.com>
2020-05-08 19:42:04 +0530

    Adding non_archived scopes and removing unnecessary classes

--- app/helpers/indents_helper.rb
+++ app/helpers/indents_helper.rb
@@ -1,10 +1,10 @@
 module IndentsHelper
   def warehouse_options(object)
-    Company.find_by(id: object.company_id)&.warehouses&.pluck(:name, :id).to_a
+    Company.find_by(id: object.company_id)&.warehouses&.non_archived&.pluck(:name, :id).to_a
   end
 
   def make_options(object)
-    Item.find_by(id: object.item_id)&.makes&.included_resources.to_a.map { |make| [make.brand_with_cat_no, make.id] }
+    Item.find_by(id: object.item_id)&.makes&.non_archived&.included_resources.to_a.map { |make| [make.brand_with_cat_no, make.id] }
   end
 
   def uom_options(object)
