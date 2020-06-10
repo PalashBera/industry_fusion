7eb84ac58c859981f156a3ba617227ce94ee0a3b
ARNAB ROY <35267739+arnabry11@users.noreply.github.com>
2020-06-10 12:22:49 +0530

    Auto assigning financial year starting and ending months (#90)

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -32,7 +32,7 @@ class Organization < ApplicationRecord
   private
 
   def validate_fy_range
-    return if fy_start_month + fy_end_month == 13
+    return if (fy_start_month == 1 && fy_end_month == 12) || (fy_start_month - fy_end_month == 1)
 
     errors.add(:base, "Invaid financial year selection")
   end
