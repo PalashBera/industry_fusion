812f118baa3edd559091de5dd87457072e8d586b
Palash Bera <palashbera1234@gmail.com>
2020-06-14 01:01:38 +0530

    Adding priority filter to reorder level

--- app/models/reorder_level.rb
+++ app/models/reorder_level.rb
@@ -20,6 +20,10 @@ class ReorderLevel < ApplicationRecord
   has_paper_trail ignore: %i[created_at updated_at]
 
   def self.included_resources
-    includes(:warehouse, :item)
+    includes(:warehouse, { item: :uom })
+  end
+
+  def quantity_with_uom
+    "#{quantity} #{item.uom.short_name}"
   end
 end
