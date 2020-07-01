3fdbbae8ef92be744683ae9691b5c56c9c9335e2
Palash Bera <palashbera1234@gmail.com>
2020-07-01 20:41:52 +0530

    [#173549786] Adding helper method for borrder bottom

--- app/controllers/transactions/indents_controller.rb
+++ app/controllers/transactions/indents_controller.rb
@@ -2,7 +2,7 @@ class Transactions::IndentsController < Transactions::HomeController
   def index
     @search = IndentItem.joins(:indent).ransack(params[:q])
     indent_items = @search.result
-    @bordered_items = indent_items.group_by(&:indent_id).map { |_k, v| v.last.id }
+    @bordered_item_ids = indent_items.group_by(&:indent_id).map { |_k, v| v.last.id }
     @pagy, @indent_items = pagy_countless(indent_items.included_resources, link_extra: 'data-remote="true"') # have to sort by indent serial with respect to FY
   end
 
