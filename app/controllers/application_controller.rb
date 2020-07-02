8e9a33f3872d76abcd1b155191d917a2f274c851
Arnab Roy <rarnab021@gmail.com>
2020-07-02 06:52:24 +0530

    Fixing email confirmation bug while changing email id

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_paper_trail_whodunnit, :set_current_user, :set_current_organization
   before_action :remove_empty_parameters, only: :index
-  before_action :redirect_to_subdomain, if: :user_signed_in?
+  before_action :redirect_to_subdomain, unless: :devise_controller?
 
   def route_not_found
     render file: Rails.public_path.join("404.html"), status: :not_found, layout: false
