16cb43508efacf681c72ce3ea0c45db24581bd67
Jhumur Chatterjee <44602220+JhumurChatterjee@users.noreply.github.com>
2020-07-05 19:15:48 +0530

    [#173549763] Adding hsn code to item groups (#119)

--- app/models/item_group.rb
+++ app/models/item_group.rb
@@ -5,10 +5,14 @@ class ItemGroup < ApplicationRecord
   include UserTrackingModule
 
   acts_as_tenant(:organization)
+  
+  before_validation { self.hsn_code = hsn_code.to_s.squish.upcase }
 
   belongs_to :organization
 
   has_many :items
 
   has_paper_trail ignore: %i[created_at updated_at]
+
+  validates :hsn_code, length: { minimum: 6, maximum: 8 }
 end
