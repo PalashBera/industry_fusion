54b30a9aa5e333ac0c11a6dc23d2c37c1e720abc
Arnab Roy <rarnab021@gmail.com>
2020-07-30 08:49:06 +0530

    Adding bulk action for approved indents

--- app/javascript/indents.js
+++ app/javascript/indents.js
@@ -1,3 +1,30 @@
+$(document).on('click', '.bulk-action-checkbox', function (e) {
+  var checkedCount = $('.bulk-action-checkbox:checked').length;
+  var totalCount = $('.bulk-action-checkbox').length;
+
+  if (checkedCount > 0) {
+    $('.bulk-action-dropdown').removeClass('hide')
+  } else {
+    $('.bulk-action-dropdown').addClass('hide')
+  }
+
+  if (totalCount === checkedCount) {
+    $('.bulk-action-select-all-checkbox').prop('checked', true);
+  } else {
+    $('.bulk-action-select-all-checkbox').prop('checked', false);
+  }
+});
+
+$(document).on('click', '.bulk-action-select-all-checkbox', function (e) {
+  if ($(this).is(":checked") === true) {
+    $('.bulk-action-checkbox').prop('checked', true)
+    $('.bulk-action-dropdown').removeClass('hide')
+  } else {
+    $('.bulk-action-checkbox').prop('checked', false)
+    $('.bulk-action-dropdown').addClass('hide')
+  }
+});
+
 $(document).on('change', '#indent_company_id', function (e) {
   var companyId = $(this).find(':selected').val();
 
