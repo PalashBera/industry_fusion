445233b63b1285b5566585aa333c9bdead481bec
Palash Bera <palashbera1234@gmail.com>
2020-08-22 20:13:41 +0530

    [#174303943] Sanitizing parameters for approval request

--- app/controllers/procurement/approval_requests/indents_controller.rb
+++ app/controllers/procurement/approval_requests/indents_controller.rb
@@ -12,12 +12,22 @@ class Procurement::ApprovalRequests::IndentsController < Procurement::ApprovalRe
     approval_request = indent_item.approval_request
 
     if approval_request
-      approval_request_user = approval_request.approval_request_users.user_filter(current_user.id).first
-      falsh_message = approval_request_user.public_send(params[:type])
+      @approval_request_user = approval_request.approval_request_users.user_filter(current_user.id).first
+      falsh_message = generate_message
     else
       falsh_message = "Indent has been #{approval_request.action_type} already."
     end
 
     redirect_to procurement_approval_requests_indents_path, flash: { success: falsh_message }
+  end
+
+  private
+
+  def generate_message
+    if %w[approve reject].include?(params[:type])
+      @approval_request_user.public_send(params[:type])
+    else
+      "You tried with unpermitted parameters."
+    end
   end
 end
