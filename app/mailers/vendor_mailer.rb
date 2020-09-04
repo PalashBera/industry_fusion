21f23ed18177ff69c9e6ea83d23fb5bdebf398b9
Palash Bera <palashbera1234@gmail.com>
2020-09-05 02:33:19 +0530

    [#174651811] Adding vendorship activation and deactivation email

--- app/mailers/vendor_mailer.rb
+++ app/mailers/vendor_mailer.rb
@@ -2,6 +2,18 @@ class VendorMailer < ApplicationMailer
   def organization_acknowledgement(vendor_id, user_id)
     @vendor = Vendor.find(vendor_id)
     @user = User.find(user_id)
-    mail to: @vendor.email, subject: I18n.t("mail.new_vendorship.subject", name: @user.organization.name)
+    mail to: @vendor.email, subject: I18n.t("mail.vendorship.new.subject", name: @user.organization.name)
+  end
+
+  def vendorship_activation_acknowledgement(vendor_id, user_id)
+    @vendor = Vendor.find(vendor_id)
+    @user = User.find(user_id)
+    mail to: @vendor.email, subject: I18n.t("mail.vendorship.activate.subject", name: @user.organization.name)
+  end
+
+  def vendorship_deactivation_acknowledgement(vendor_id, user_id)
+    @vendor = Vendor.find(vendor_id)
+    @user = User.find(user_id)
+    mail to: @vendor.email, subject: I18n.t("mail.vendorship.archive.subject", name: @user.organization.name)
   end
 end
