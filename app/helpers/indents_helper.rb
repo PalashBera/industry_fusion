3b433b0ecb786a2e77ac50a292b430231ad96d2c
Arnab Roy <rarnab021@gmail.com>
2020-09-05 13:04:16 +0530

    [#174587269] Add drop down helper for master section

--- app/helpers/indents_helper.rb
+++ app/helpers/indents_helper.rb
@@ -1,85 +1,50 @@
 module IndentsHelper
-  def company_dropdown_list(object)
-    companies = Company.warehouse_filter(accessible_warehouse_ids).non_archived
-    dropdown_list = companies.pluck(:name, :id)
-    return dropdown_list unless object.company_id
+  def method_missing(method, *args, &block)
+    if method.to_s =~ /^dropdown_list_\w+/
+      self.class.__send__(:define_method, method) do |*args|
+        method_definition(method, args)
+      end
 
-    unless companies.pluck(:id).include?(object.company_id)
-      selected_company = Company.find(object.company_id)
-      dropdown_list = [[selected_company.name, selected_company.id]].concat(dropdown_list)
+      __send__ method, *args, &block
+    else
+      super
     end
-
-    dropdown_list
   end
 
-  def warehouse_dropdown_list(object)
-    return [] unless object.company_id
+  def respond_to_missing?(method_name, include_private = false)
+    method_name.to_s.start_with?("dropdown_list_") || super
+  end
 
-    warehouses = Warehouse.id_filter(accessible_warehouse_ids).company_filter(object.company_id).non_archived.order_by_name
-    dropdown_list = warehouses.pluck(:name, :id)
+  def method_definition(method, args)
+    klass_name = method.to_s.gsub(/^dropdown_list_/, "")
+    obj, attribute = args
+    klass_name_id = obj.public_send("#{klass_name}_id")
+    build_dropdown_list(klass_name, klass_name_id, attribute, obj)
+  end
 
-    unless warehouses.pluck(:id).include?(object.warehouse_id)
-      selected_warehouse = Warehouse.find(object.warehouse_id)
-      dropdown_list = [[selected_warehouse.name, selected_warehouse.id]].concat(dropdown_list)
+  def fetch_entities(klass_name, obj)
+    case klass_name
+    when "company"
+      Company.warehouse_filter(accessible_warehouse_ids).non_archived
+    when "warehouse"
+      Warehouse.id_filter(accessible_warehouse_ids).company_filter(obj.company_id).non_archived.order_by_name if obj.company_id
+    when "make"
+      Make.item_filter(obj.item_id).non_archived.included_resources if obj.item_id
+    when "uom"
+      Item.find_by(id: obj.item_id).uoms if obj.item_id
+    else
+      klass_name.camelize.constantize.non_archived.order_by_name
     end
-
-    dropdown_list
   end
 
-  def indentor_dropdown_list(object)
-    indentors = Indentor.non_archived.order_by_name
-    dropdown_list = indentors.pluck(:name, :id)
-    return dropdown_list unless object.indentor_id
+  def build_dropdown_list(klass_name, klass_name_id, attribute, obj)
+    entities = fetch_entities(klass_name, obj) || []
+    dropdown_list = entities.map { |ent| [ent.public_send(attribute), ent.id] }
+    return dropdown_list unless klass_name_id
 
-    if object.indentor_id && !indentors.pluck(:id).include?(object.indentor_id)
-      selected_indentor = Indentor.find(object.indentor_id)
-      dropdown_list = [[selected_indentor.name, selected_indentor.id]].concat(dropdown_list)
-    end
-
-    dropdown_list
-  end
-
-  def item_dropdown_list(object)
-    items = Item.non_archived.order_by_name
-    dropdown_list = items.pluck(:name, :id)
-    return dropdown_list unless object.item_id
-
-    unless items.pluck(:id).include?(object.item_id)
-      selected_item = Item.find(object.item_id)
-      dropdown_list = [[selected_item.name, selected_item.id]].concat(dropdown_list)
-    end
-
-    dropdown_list
-  end
-
-  def make_dropdown_list(object)
-    return [] unless object.item_id
-
-    makes = Make.item_filter(object.item_id).non_archived.included_resources
-    dropdown_list = makes.map { |make| [make.brand_with_cat_no, make.id] }
-
-    if object.make_id && !makes.pluck(:id).include?(object.make_id)
-      selected_make = Make.find(object.make_id)
-      dropdown_list = [[selected_make.brand_with_cat_no, selected_make.id]].concat(dropdown_list)
-    end
-
-    dropdown_list
-  end
-
-  def uom_dropdown_list(object)
-    return [] unless object.item_id
-
-    Item.find_by(id: object.item_id).uoms.map { |uom| [uom.short_name, uom.id] }
-  end
-
-  def cost_center_dropdown_list(object)
-    cost_centers = CostCenter.non_archived.order_by_name
-    dropdown_list = cost_centers.pluck(:name, :id)
-    return dropdown_list unless object.cost_center_id
-
-    unless cost_centers.pluck(:id).include?(object.cost_center_id)
-      selected_cost_center = CostCenter.find(object.cost_center_id)
-      dropdown_list = [[selected_cost_center.name, selected_cost_center.id]].concat(dropdown_list)
+    unless entities.map(&:id).include?(klass_name_id)
+      selected_entity = klass_name.camelize.constantize.find(klass_name_id)
+      dropdown_list = [[selected_entity.public_send(attribute), selected_entity.id]].concat(dropdown_list)
     end
 
     dropdown_list
