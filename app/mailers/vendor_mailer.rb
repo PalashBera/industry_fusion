1b27237e2efca63d4750b70eb21298cee686c9c6
Arnab Roy <rarnab021@gmail.com>
2020-09-30 08:42:56 +0530

    [#174842354] Sending email notification to vendors on receiving a quotation request

--- app/mailers/vendor_mailer.rb
+++ app/mailers/vendor_mailer.rb
@@ -16,4 +16,10 @@ class VendorMailer < ApplicationMailer
     @user = User.find(user_id)
     mail to: @vendor.email, subject: I18n.t("mail.vendorship.archive.subject", name: @user.organization.name)
   end
+
+  def quotation_request_mail(vendor_id, quotation_request_id)
+    @quotation_request = QuotationRequest.find(quotation_request_id)
+    @vendor = Vendor.find(vendor_id)
+    mail to: @vendor.email, subject: I18n.t("mail.vendor.quotation_request.subject", serial: @quotation_request.serial_number)
+  end
 end
